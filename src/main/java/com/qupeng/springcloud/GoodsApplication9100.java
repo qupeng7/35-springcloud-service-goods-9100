77466fb4f24e5bc062e7c98ec752f7cee210aea3
qupeng <qupeng7@126.com>
2019-12-19 11:45:44 +0800

    commit

--- src/main/java/com/qupeng/springcloud/GoodsApplication9100.java
+++ src/main/java/com/qupeng/springcloud/GoodsApplication9100.java
@@ -1,9 +1,11 @@
 package com.qupeng.springcloud;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+//@EnableApolloConfig
 @EnableEurekaClient
 @SpringBootApplication
 public class GoodsApplication9100 {
